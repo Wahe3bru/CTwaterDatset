074984175f08c539dee0317347879e121a1ceb4f
Waheeb Agherdien <wahe3bru@gmail.com>
2017-11-18 21:19:38 +0200

    placed bar graph in div

--- web/app.js
+++ web/app.js
@@ -18,7 +18,7 @@ var tooltip = d3.select("body")
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
 // move the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#South_suburbs").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
